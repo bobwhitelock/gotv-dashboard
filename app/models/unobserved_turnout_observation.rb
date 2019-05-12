6de3687689f43dfafc114df78bf9822ac77491a9
Bob Whitelock <bob.whitelock1@gmail.com>
2019-05-12 21:22:07 +0100

    Add method for latest observations for all PollingStations

---
+++ app/models/unobserved_turnout_observation.rb
@@ -0,0 +1,9 @@
+
+# Null object TurnoutObservation for not yet observed PollingStation.
+class UnobservedTurnoutObservation
+  def created_at; end
+
+  def count
+    0
+  end
+end
---
+++ spec/models/work_space_spec.rb
@@ -0,0 +1,50 @@
+
+require 'rails_helper'
+
+RSpec.describe WorkSpace do
+  describe '#latest_observations' do
+    subject do
+      create(
+        :work_space,
+        polling_stations: [polling_station],
+      )
+    end
+    let :polling_station do
+      create(:polling_station, name: '10 Downing Street')
+    end
+
+    it 'gives most recent turnout observation for each polling station' do
+      another_observation = create(
+        :turnout_observation,
+        polling_station: polling_station,
+        count: 11,
+        created_at: 2.hours.ago)
+      most_recent_observation = create(
+        :turnout_observation,
+        polling_station: polling_station,
+        count: 22,
+        created_at: 1.hour.ago
+      )
+      subject.turnout_observations = [
+        most_recent_observation,
+        another_observation,
+      ]
+      subject.save!
+
+      data = subject.latest_observations
+
+      first_entry = data.first
+      expect(first_entry.polling_station).to eq(polling_station)
+      expect(first_entry.turnout_observation).to eq(most_recent_observation)
+    end
+
+    it 'gives placeholder empty observation for polling station without observation' do
+      data = subject.latest_observations
+
+      first_entry = data.first
+      expect(first_entry.polling_station).to eq(polling_station)
+      expect(first_entry.turnout_observation.count).to eq(0)
+      expect(first_entry.turnout_observation.created_at).to be_nil
+    end
+  end
+end
