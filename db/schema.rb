605f1563cfe22f67fc6471e8e94488ee9c697714
Bob Whitelock <bob.whitelock1@gmail.com>
2019-05-20 23:57:39 +0100

    Add Devise for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_002432) do
+ActiveRecord::Schema.define(version: 2019_05_20_224937) do
 
   create_table "audits", force: :cascade do |t|
     t.integer "auditable_id"
@@ -73,6 +73,18 @@ ActiveRecord::Schema.define(version: 2019_05_20_002432) do
     t.integer "work_space_id"
     t.index ["polling_station_id"], name: "index_turnout_observations_on_polling_station_id"
     t.index ["work_space_id"], name: "index_turnout_observations_on_work_space_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "wards", force: :cascade do |t|
