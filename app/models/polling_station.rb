83050a0afbe7064bd9599717faf23542fa2fdffd
Bob Whitelock <bob.whitelock1@gmail.com>
2019-05-20 01:13:03 +0100

    Display ward wherever we display polling station (fixes #18)
    Show this as a new, initial, column in the two tables, and in brackets
    otherwise.

--- app/models/polling_station.rb
+++ app/models/polling_station.rb
@@ -3,4 +3,9 @@ class PollingStation < ApplicationRecord
   has_many :confirmed_labour_voter_observations
   has_many :turnout_observations
   belongs_to :ward
+
+  # XXX move to decorator?
+  def name_with_ward
+    "#{name} (#{ward.name})"
+  end
 end
