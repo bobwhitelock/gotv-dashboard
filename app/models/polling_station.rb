33dcd42c0f01ac982a1dd5c117e4df1ba6962a4c
Bob Whitelock <bob.whitelock1@gmail.com>
2019-11-10 00:32:18 +0000

    Tweak solution for #47 following data model updates

--- app/models/polling_station.rb
+++ app/models/polling_station.rb
@@ -9,13 +9,4 @@ class PollingStation < ApplicationRecord
   def fully_specified_name
     "#{reference}: #{name} (#{ward.name})"
   end
-
-  def colocated_polling_stations
-    # Try to merge equivalent postcodes, and reject the polling station we are looking at.
-    PollingStation.all.select{ |ps| ps.id != id and ps.postcode.upcase.gsub(/\s+/, "") == postcode.upcase.gsub(/\s+/, "") }
-  end
-
-  def last_observation
-    turnout_observations.max_by{ |ob| ob.created_at }
-  end
 end
--- app/models/work_space_polling_station.rb
+++ app/models/work_space_polling_station.rb
@@ -4,12 +4,25 @@ class WorkSpacePollingStation < ApplicationRecord
   has_one :ward, through: :polling_station
   has_many :turnout_observations
 
-  delegate :reference, :name, :fully_specified_name, to: :polling_station
+  delegate :reference,
+    :name,
+    :postcode,
+    :fully_specified_name,
+    to: :polling_station
 
   validates_presence_of :pre_election_labour_promises
   validates_presence_of :pre_election_registered_voters
 
   def as_json(options = {})
     super(options).merge(fully_specified_name: fully_specified_name)
+  end
+
+  def colocated_polling_stations
+    # Try to merge equivalent postcodes, and reject the polling station we are looking at.
+    work_space.work_space_polling_stations.select{ |ps| ps.id != id and ps.postcode.upcase.gsub(/\s+/, "") == postcode.upcase.gsub(/\s+/, "") }
+  end
+
+  def last_observation
+    turnout_observations.max_by{ |ob| ob.created_at }
   end
 end
