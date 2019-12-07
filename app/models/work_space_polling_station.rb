3f47b5249353cd20fa4b4888c5be9a8c24b7a622
Bob Whitelock <bob.whitelock1@gmail.com>
2019-12-07 21:16:36 +0000

    Extract method for getting WSPSs for district

--- app/models/work_space_polling_station.rb
+++ app/models/work_space_polling_station.rb
@@ -33,20 +33,21 @@ class WorkSpacePollingStation < ApplicationRecord
     turnout_observations.max_by(&:created_at)
   end
 
-  # XXX Existence of this method and all usage is a total hack - we use first
-  # WorkSpacePollingStation for PollingDistrict as a proxy for a
+  # XXX Existence of these 2 methods and all usage is a total hack - we use
+  # first WorkSpacePollingStation for PollingDistrict as a proxy for a
   # 'WorkSpacePollingDistrict', to work around this not existing and being
   # involved to add. Should fix at some point post-election -
   # https://github.com/bobwhitelock/gotv-dashboard/issues/100.
   def work_space_polling_district_proxy?
-    polling_district_work_space_stations = \
-      WorkSpacePollingStation.joins(
-        :polling_station
+    work_space_polling_district_stations.first == self
+  end
+
+  def work_space_polling_district_stations
+    WorkSpacePollingStation.joins(
+      :polling_station
     ).where(
       polling_stations: { polling_district: polling_district },
       work_space: work_space
     ).order(:reference)
-
-    polling_district_work_space_stations.first == self
   end
 end
