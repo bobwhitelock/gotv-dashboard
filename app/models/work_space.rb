87e7f96b62240c41893e89e97f42194d127d635c
Bob Whitelock <bob.whitelock1@gmail.com>
2019-12-08 23:00:08 +0000

    Add explicit ordering to WorkSpace `through` associations
    So when we allow selecting these they are always alphabetically ordered.

--- app/models/work_space.rb
+++ app/models/work_space.rb
@@ -3,8 +3,8 @@ class WorkSpace < ApplicationRecord
   has_many :committee_rooms
   has_many :turnout_observations, through: :work_space_polling_stations
   has_many :polling_stations, through: :work_space_polling_stations
-  has_many :wards, -> { distinct }, through: :polling_stations
-  has_many :polling_districts, -> { distinct }, through: :polling_stations
+  has_many :wards, -> { distinct.order(:name) }, through: :polling_stations
+  has_many :polling_districts, -> { distinct.order(:reference) }, through: :polling_stations
 
   accepts_nested_attributes_for :work_space_polling_stations
 
