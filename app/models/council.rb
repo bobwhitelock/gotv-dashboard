a9a156bdc236bce5ff3edb55dd833f00c716bf20
Bob Whitelock <bob.whitelock1@gmail.com>
2019-12-09 23:17:41 +0000

    Allow accessing PollingDistricts for Council

--- app/models/council.rb
+++ app/models/council.rb
@@ -1,5 +1,6 @@
 class Council < ApplicationRecord
   has_many :wards
+  has_many :polling_districts, through: :wards
   has_many :polling_stations, through: :wards
 
   # `transient` flag indicates that a Council is a fake Council just for easy
