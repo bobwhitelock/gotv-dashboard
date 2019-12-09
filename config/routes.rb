81afa977c15ef699db1b588191922220a1d9b1f6
Bob Whitelock <bob.whitelock1@gmail.com>
2019-12-09 03:05:16 +0000

    Add route to test Sentry integration

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   root 'pages#landing'
 
+  get 'test-sentry' => 'pages#test_sentry'
+
   get 'start' => 'work_spaces#start'
   post 'demo' => 'work_spaces#demo'
 
