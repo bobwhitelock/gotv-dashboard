fdd707bfc1b4091d0a394242ecda4866e430f418
Bob Whitelock <bob.whitelock1@gmail.com>
2019-12-10 14:57:59 +0000

    Note difference between WARP counts and other observations

--- app/models/warp_count_observation.rb
+++ app/models/warp_count_observation.rb
@@ -5,5 +5,11 @@ class WarpCountObservation < ApplicationRecord
   belongs_to :work_space_polling_station
   belongs_to :user
 
+  # Note: Unlike other observations, WARP counts are additive rather than a
+  # snapshot of the latest state for some statistic in some area. This makes
+  # more sense in this case as organisers want to be able to put in the count
+  # from WARP of all the new confirmed votes in an area, from the latest
+  # canvassing, and then the total count will be the sum of all the (valid)
+  # such counts throughout the day.
   validates_presence_of :count
 end
