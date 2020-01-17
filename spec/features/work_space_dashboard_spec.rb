d78708e583bc2e22a27b66b312c62e13d9fe1fc8
Bob Whitelock <bob.whitelock1@gmail.com>
2020-01-17 02:19:27 +0000

    Add feature tests for remaining lifts tracking

--- spec/features/work_space_dashboard_spec.rb
+++ spec/features/work_space_dashboard_spec.rb
@@ -28,4 +28,53 @@ RSpec.feature 'work space dashboard', type: :feature, js: true do
     expect(page).to have_text('20 promises / 60 postal voters')
     expect(page).to have_text('0 (not yet observed)')
   end
+
+  describe 'remaining lifts tracking' do
+    before :each do
+      @polling_station = create(:work_space_polling_station)
+
+      visit work_space_path(@polling_station.work_space)
+
+      @count_element = find_data_test(
+        "remaining-lifts-#{@polling_station.polling_district.id}"
+      )
+    end
+
+    it 'initializes count at 0' do
+      expect(@count_element).to have_text('0')
+    end
+
+    it 'allows increasing and decreasing count' do
+      increase_button = find_data_test('increase-button', root: @count_element)
+      decrease_button = find_data_test('decrease-button', root: @count_element)
+
+      increase_button.click
+      expect(@count_element).to have_text('1')
+
+      decrease_button.click
+      expect(@count_element).to have_text('0')
+    end
+
+    it 'does not allow count to go below 0' do
+      decrease_button = find_data_test('decrease-button', root: @count_element)
+
+      decrease_button.click
+
+      expect(@count_element).to have_text('0')
+    end
+
+    it 'persists count to server once unchanged for brief period' do
+      increase_button = find_data_test('increase-button', root: @count_element)
+
+      3.times { increase_button.click }
+      sleep 1 # Allow JS time to make AJAX request.
+      visit work_space_path(@polling_station.work_space)
+
+      expect(@count_element).to have_text('3')
+      remaining_lifts_observations =
+        @polling_station.remaining_lifts_observations
+      expect(remaining_lifts_observations.length).to eq 1
+      expect(remaining_lifts_observations.first.count).to eq 3
+    end
+  end
 end
--- spec/support/capybara_utilities.rb
+++ spec/support/capybara_utilities.rb
@@ -3,4 +3,11 @@ module CapybaraUtilities
   def send_keys(*keys)
     find('body').send_keys(*keys)
   end
+
+  # Find element with given `value` for `data-test` attribute (these attributes
+  # are intended only for finding elements in tests), relative to element
+  # passed as `root` (defaulting to entire page).
+  def find_data_test(value, root: page)
+    root.find("[data-test=\"#{value}\"]")
+  end
 end
