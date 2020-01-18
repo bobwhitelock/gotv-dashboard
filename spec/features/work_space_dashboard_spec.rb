e85414bac7cdbfd2b9f2cf6b0f486c25ca30812c
Bob Whitelock <bob.whitelock1@gmail.com>
2020-01-18 23:23:09 +0000

    Add feature tests for canvassers and cars tracking
    Currently with a lot of duplication with remaining lifts tracking tests,
    to be refactored.

--- spec/features/work_space_dashboard_spec.rb
+++ spec/features/work_space_dashboard_spec.rb
@@ -80,4 +80,120 @@ RSpec.feature 'work space dashboard', type: :feature, js: true do
       expect(remaining_lifts_observations.first.count).to eq 3
     end
   end
+
+  describe 'canvassers tracking' do
+    before :each do
+      visit work_space_path(committee_room.work_space)
+    end
+
+    let :committee_room do
+      work_space = create(:work_space)
+      create(
+        :committee_room,
+        work_space: work_space,
+        work_space_polling_stations: [
+          create(:work_space_polling_station, work_space: work_space)
+        ]
+      )
+    end
+
+    let :count_element do
+      find_data_test("canvassers-#{committee_room.id}")
+    end
+
+    let :increase_button do
+      find_data_test('increase-button', root: count_element)
+    end
+
+    let :decrease_button do
+      find_data_test('decrease-button', root: count_element)
+    end
+
+    it 'initializes count at 0' do
+      expect(count_element).to have_text('0')
+    end
+
+    it 'allows increasing and decreasing count' do
+      increase_button.click
+      expect(count_element).to have_text('1')
+
+      decrease_button.click
+      expect(count_element).to have_text('0')
+    end
+
+    it 'does not allow count to go below 0' do
+      decrease_button.click
+
+      expect(count_element).to have_text('0')
+    end
+
+    it 'persists count to server once unchanged for brief period' do
+      3.times { increase_button.click }
+      sleep 1 # Allow JS time to make AJAX request.
+      visit work_space_path(committee_room.work_space)
+
+      expect(count_element).to have_text('3')
+      canvassers_observations = committee_room.canvassers_observations
+      expect(canvassers_observations.length).to eq 1
+      expect(canvassers_observations.first.count).to eq 3
+    end
+  end
+
+  describe 'cars tracking' do
+    before :each do
+      visit work_space_path(committee_room.work_space)
+    end
+
+    let :committee_room do
+      work_space = create(:work_space)
+      create(
+        :committee_room,
+        work_space: work_space,
+        work_space_polling_stations: [
+          create(:work_space_polling_station, work_space: work_space)
+        ]
+      )
+    end
+
+    let :count_element do
+      find_data_test("cars-#{committee_room.id}")
+    end
+
+    let :increase_button do
+      find_data_test('increase-button', root: count_element)
+    end
+
+    let :decrease_button do
+      find_data_test('decrease-button', root: count_element)
+    end
+
+    it 'initializes count at 0' do
+      expect(count_element).to have_text('0')
+    end
+
+    it 'allows increasing and decreasing count' do
+      increase_button.click
+      expect(count_element).to have_text('1')
+
+      decrease_button.click
+      expect(count_element).to have_text('0')
+    end
+
+    it 'does not allow count to go below 0' do
+      decrease_button.click
+
+      expect(count_element).to have_text('0')
+    end
+
+    it 'persists count to server once unchanged for brief period' do
+      3.times { increase_button.click }
+      sleep 1 # Allow JS time to make AJAX request.
+      visit work_space_path(committee_room.work_space)
+
+      expect(count_element).to have_text('3')
+      cars_observations = committee_room.cars_observations
+      expect(cars_observations.length).to eq 1
+      expect(cars_observations.first.count).to eq 3
+    end
+  end
 end
